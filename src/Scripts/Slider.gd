b6d7209f3435a5891b54b2af1e20ad3d56f8b7b7
Tobybsmith <tobybsmith121@gmail.com>
2023-01-21 14:20:22 -0500

    Improved Slider

--- src/Scripts/Slider.gd
+++ src/Scripts/Slider.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var start_pos = Vector2()
+var current_pos = Vector2()
 var start_rot
 
 var pos
@@ -14,22 +15,24 @@ func _ready():
 	start_pos = get_node("SliderKnob").global_position
 	set_process(false)
 
-func _on_SliderKnob_input_event(viewport, event, shape_idx):
+func _on_Slider_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("click"):
-		start_pos = get_global_mouse_position()
+		start_pos = get_node("SliderKnob").global_position
+		print("START POS: " + str(start_pos))
 		set_process(true)
 
 func _process(delta):
-	pos = start_pos.y - get_global_mouse_position().y
+	pos = (get_global_mouse_position().y - start_pos.y)
 	
 	if pos < min_pos:
 		pos = min_pos
 	elif pos > max_pos:
 		pos = max_pos
 	
-	norm_pos = pos / max_pos
+	norm_pos = 1 - (pos / max_pos)
 	
-	get_node("SliderKnob").global_position = Vector2(get_node("SliderKnob").global_position.x, pos);
+	get_node("SliderKnob").global_position.y = pos + max_pos
 	
 	if Input.is_action_just_released("click"):
+		print("END POS: " + str(get_node("SliderKnob").global_position))
 		set_process(false)
