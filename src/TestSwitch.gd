b50d4d151f5db7c8ee0f546830f323bdc31b0c89
David <david.martinez@spaceconcordia.ca>
2023-01-21 12:05:28 -0500

    Cockpits created, Audiostream player added

--- src/TestSwitch.gd
+++ src/TestSwitch.gd
@@ -21,3 +21,29 @@ func _on_TestSwitch_input_event(viewport, event, shape_idx):
 			print("Pressed")
 		
 		IsPressed = not IsPressed
+		
+		# Create a variable to store the audio stream player
+var audio_scratch = AudioStreamPlayer.new()
+audio_scratch.set_stream("res://scratch.ogg")
+
+# Create a variable to store the current track
+var current_track = AudioStreamPlayer.new()
+current_track.set_stream("res://track.ogg")
+current_track.play()
+
+# Create a variable to store the steering wheel image
+var steering_wheel = TextureButton.new()
+steering_wheel.set_normal_texture("res://steering_wheel.png")
+steering_wheel.set_position(Vector2(50, 50))
+
+# Connect the steering wheel's pressed signal to a function
+steering_wheel.connect("pressed", self, "_on_steering_wheel_pressed")
+add_child(steering_wheel)
+
+# Create the function that will handle the steering wheel's pressed signal
+func _on_steering_wheel_pressed():
+	# Play the scratch sound effect
+	audio_scratch.play()
+	# Scratch the current track
+	current_track.seek(current_track.get_playback_position() + 0.1)
+
