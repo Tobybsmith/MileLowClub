8e21245cd03c62b6739f175abfb696ebb77a48d8
David <david.martinez@spaceconcordia.ca>
2023-01-21 17:00:51 -0500

    Fixed Button names

---
+++ src/Reverb.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var down_sprite = load("res://images/button_img_1_p.png")
+var up_sprite = load("res://images/button_img_1_u.png")
+
+var IsPressed = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+		pass # Replace with function body
+ 
+func _on_Reverb_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		IsPressed = not IsPressed
+		if IsPressed:
+			#Update sprite here
+			get_node("Sprite").texture = down_sprite
+			print("Reverb Enabled")
+		else:
+			#Update sprite here
+			get_node("Sprite").texture = up_sprite
+			print("Reverb Disabled")
