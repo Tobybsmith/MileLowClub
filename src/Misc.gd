d597b52a780a9f0f4874969f1b4a9ea438b4c1f1
Tobybsmith <tobybsmith121@gmail.com>
2023-01-21 20:49:42 -0500

    Final background update

--- src/Misc.gd
+++ src/Misc.gd
@@ -3,6 +3,10 @@ extends Node2D
 var rng = RandomNumberGenerator.new()
 var IsPressed = false
 
+var dir = 1
+var rot = 0
+var speed = 0.5
+
 var down = load("res://images/Submit_p.jpeg")
 var up = load("res://images/Submit_u.jpeg")
 
@@ -13,17 +17,26 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	rot += speed * dir
+	print(rot)
+	if rot > 20:
+		dir = -1
+	if rot < 0:
+		dir = 1
+	get_node("Spotlight1").rotation_degrees = rot / 1.1
+	get_node("Spotlight2").rotation_degrees = rot / 0.9
+	pass
 
 
 func _on_ColorLights_input_event(viewport, event, shape_idx):
 	#select a random color
 	if event is InputEventMouseButton and event.pressed:
-		if IsPressed:
-			get_parent().get_node("ColorLights/Sprite").texture = down
-		else: 
-			get_parent().get_node("ColorLights/Sprite").texture = up
-		IsPressed = not IsPressed
-		get_node("Spotlight1").modulate = Color(rng.randf(), rng.randf(), rng.randf())
-		get_node("Spotlight2").modulate = Color(rng.randf(), rng.randf(), rng.randf())
+		get_parent().get_node("ColorLights/Sprite").texture = down
+		get_parent().get_node("ColorLights/Timer").start()
+		get_node("Spotlight1").modulate = Color(rng.randf_range(0.5,1.0), rng.randf_range(0.5,1.0), rng.randf_range(0.5,1.0))
+		get_node("Spotlight2").modulate = Color(rng.randf_range(0.5,1.0), rng.randf_range(0.5,1.0), rng.randf_range(0.5,1.0))
+
+
+func _on_Timer_timeout():
+	get_parent().get_node("ColorLights/Sprite").texture = up
