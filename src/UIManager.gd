d839eb82439c5c344a06cb4654d1f64469e92b48
Tobybsmith <tobybsmith121@gmail.com>
2023-01-21 23:30:12 -0500

    Added basic queue support for left-monitor

---
+++ src/UIManager.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func music_dir_loaded(music_queue):
+	#Kill the old stuff, and add the music queue to this area
+	get_tree().call_group("left-monitor", "queue_free")
+	#Print the queue to the screen
+	get_node("MusicFinder/PathLabel").text = "Queue:"
+	for song in music_queue:
+		get_node("MusicFinder/QueueContentsLabel").text += song + "\n"
